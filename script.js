5e0d1c110c6f1ede90784708d25db6228daff61e
Isaac orija <isaac.orija@gmail.com>
2021-04-06 12:56:14 +0100

    testy

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let deposit = document.getElementById('deposit')
+function closeDeposit(){
+    let deposit = document.getElementById('deposit')
 
 deposit.addEventListener('click', function(){
     document.getElementById("overlay").style.display = "block";
@@ -8,3 +9,17 @@ deposit.addEventListener('click', function(){
 document.getElementById("close").addEventListener('click', function(){
      document.getElementById("overlay").style.display = "none";
 })
+}
+closeDeposit()
+
+function copyValue(){
+    let copyIcon = document.getElementById('copied')
+    let inputValue = document.getElementById('wallet-input').value;
+
+copyIcon.addEventListener('click', function(){
+    alert(`${inputValue} copied`)
+})
+
+}
+
+copyValue()
